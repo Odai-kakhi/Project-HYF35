f6f2294d88cd9b5ff4054479455dc961e34c6d46
jalal75 <jalal.alnadeem@gmail.com>
2017-10-01 00:42:24 +0200

    create the Load screen from  the explorer storage

--- src/components/SaveScreen.js
+++ src/components/SaveScreen.js
@@ -18,28 +18,28 @@ export default class SaveScreen extends React.Component {
   changeProgramScreen(screenName) {
     console.log(screenName)
     store.setState({
-      programScreen : screenName
+      programScreen: screenName
     })
   }
   handleSave(Name, description) {
     console.log(Name, description)
     // Check browser support
-if (typeof(Storage) !== "undefined") {
-  // Store
-  if (Object.keys(localStorage).indexOf(Name) !== -1) {
-    if (window.confirm("This program is already exist, do you want to overwrite it?")) {
-      description = description + '\n\n```\n' + store.state.programText + '\n```'
-      localStorage.setItem(Name, description);
+    if (typeof (Storage) !== "undefined") {
+      // Store
+      if (Object.keys(localStorage).indexOf(Name) !== -1) {
+        if (window.confirm("This program is already exist, do you want to overwrite it?")) {
+          description = description + '\n\n```\n' + store.state.programText + '\n```'
+          localStorage.setItem(Name, description);
+        }
+      } else {
+        description = description + '\n\n```\n' + store.state.programText + '\n```'
+        localStorage.setItem(Name, description);
+      }
+
+
+    } else {
+      alert("Sorry, your browser does not support Web Storage...")
     }
-  } else {
-    description = description + '\n\n```\n' + store.state.programText + '\n```'
-    localStorage.setItem(Name, description);
-  }
-  
-  
-} else {
-  alert("Sorry, your browser does not support Web Storage...")
-}
   }
 
   render() {
@@ -47,28 +47,28 @@ if (typeof(Storage) !== "undefined") {
     let programDescription = ''
     return (
       <div>
-        <div className='SaveScreen' onClick={()=>{this.changeProgramScreen('ProgramArea')}}>
+        <div className='SaveScreen' onClick={() => { this.changeProgramScreen('ProgramArea') }}>
           Cancel
         </div>
 
 
-          <label>
+        <label>
 
-            <input type="text" className='programName' id = 'textArea'
-             onChange={(event) => {
+          <input type="text" className='programName' id='textArea' defaultValue='Name'
+            onChange={(event) => {
               programName = event.target.value
-              }
+            }
             } />
 
-            <textarea className='text-area' id = 'description' onChange={
-              (event) => {
-                programDescription = event.target.value
-              }
-            } /> 
-          </label> 
-          <button type="button" className = "run-button"
-              onClick={()=>{this.handleSave(programName, programDescription)}}
-          >
+          <textarea className='text-area' defaultValue='Description' id='description' onChange={
+            (event) => {
+              programDescription = event.target.value
+            }
+          } />
+        </label>
+        <button type="button" className="run-button"
+          onClick={() => { this.handleSave(programName, programDescription) }}
+        >
           Save
           </button>
       </div>
